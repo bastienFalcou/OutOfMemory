a657c8494c5c3649d3029f52c873f67918b5f125
Bastien Falcou <bastien.falcou@compass.com>
2019-06-23 12:06:41 -0400

    feat(AppDelegate): addition log of memory warning and trigger either analytics event or non-fatal crash

--- OutOfMemoryShowroom/AppDelegate.swift
+++ OutOfMemoryShowroom/AppDelegate.swift
@@ -15,4 +15,33 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
+
+    func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
+        var properties: [String: String] = [:]
+
+        if let footprint = memoryFootprint {
+            properties["memory"] = String(format: "%d MB", footprint / 1024 / 1024)
+        }
+
+        // Trigger "Analytics Event" or "Non-Fatal Crash"
+    }
+
+    private var memoryFootprint: mach_vm_size_t? {
+        guard let memory_offset = MemoryLayout.offset(of: \task_vm_info_data_t.min_address) else {
+            return nil
+        }
+        let TASK_VM_INFO_COUNT = mach_msg_type_number_t(MemoryLayout<task_vm_info_data_t>.size / MemoryLayout<integer_t>.size)
+        let TASK_VM_INFO_REV1_COUNT = mach_msg_type_number_t(memory_offset / MemoryLayout<integer_t>.size)
+        var info = task_vm_info_data_t()
+        var count = TASK_VM_INFO_COUNT
+        let kr = withUnsafeMutablePointer(to: &info) { infoPtr in
+            infoPtr.withMemoryRebound(to: integer_t.self, capacity: Int(count)) { intPtr in
+                task_info(mach_task_self_, task_flavor_t(TASK_VM_INFO), intPtr, &count)
+            }
+        }
+        guard kr == KERN_SUCCESS, count >= TASK_VM_INFO_REV1_COUNT else {
+            return nil
+        }
+        return info.phys_footprint
+    }
 }
