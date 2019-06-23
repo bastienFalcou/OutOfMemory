60f5e03d62579f31dbb84febdcbe39938bd26091
Bastien Falcou <bastien.falcou@compass.com>
2019-06-23 10:20:36 -0400

    feat(MenuViewController): addition table view and some initializations

--- OutOfMemoryShowroom/View Controllers/MenuViewController.swift
+++ OutOfMemoryShowroom/View Controllers/MenuViewController.swift
@@ -9,5 +9,19 @@
 import UIKit
 
 final class MenuViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+}
+
+extension MenuViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        fatalError()
+    }
+}
+
+extension MenuViewController: UITableViewDelegate {
 
 }
