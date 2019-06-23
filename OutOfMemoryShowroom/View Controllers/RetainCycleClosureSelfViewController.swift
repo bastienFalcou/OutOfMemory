3b486a8d7519e56216b9cf17eff60d84548f9643
Bastien Falcou <bastien.falcou@compass.com>
2019-06-23 10:45:43 -0400

    feat(RetainCycleClosureSelfViewController): addition new empty view controller

---
+++ OutOfMemoryShowroom/View Controllers/RetainCycleClosureSelfViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RetainCycleClosureSelfViewController.swift
+//  OutOfMemoryShowroom
+//
+//  Created by Bastien Falcou on 6/23/19.
+//  Copyright © 2019 Bastien Falcou. All rights reserved.
+//
+
+import UIKit
+
+final class RetainCycleClosureSelfViewController: UIViewController {
+
+}
