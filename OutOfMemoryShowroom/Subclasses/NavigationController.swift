1b86792442c32779ad85d205dd883587fb41d94d
Bastien Falcou <bastien.falcou@compass.com>
2019-06-23 11:46:58 -0400

    feat(NavigationController): condition on preprocessor macro to only enable fatalError on debug

--- OutOfMemoryShowroom/Subclasses/NavigationController.swift
+++ OutOfMemoryShowroom/Subclasses/NavigationController.swift
@@ -13,13 +13,15 @@ public class NavigationController: UINavigationController {
 
     public override func popViewController(animated: Bool) -> UIViewController? {
         let viewController = super.popViewController(animated: animated)
-        if isRetainCycleSafetyEnabled {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak viewController] in
-                if let viewController = viewController {
-                    fatalError("VC Not Deallocated: \(String(describing: viewController.classForCoder)))")
+        #if DEBUG
+            if isRetainCycleSafetyEnabled {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak viewController] in
+                    if let viewController = viewController {
+                        fatalError("VC Not Deallocated: \(String(describing: viewController.classForCoder)))")
+                    }
                 }
             }
-        }
+        #endif
         return viewController
     }
 }
